33ccb62a7ed11196d595dc75d1b743587d97765c
Maria Lopez I Vivas <mlopez-i@cbr2s3.42barcelona.com>
2023-05-03 18:20:08 +0200

    norminette ok

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -17,7 +17,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	char	*s;
 
 	s = (char *)b;
-	while(len > 0)
+	while (len > 0)
 	{
 		s[len - 1] = c;
 		len--;
--- libft/libft.h
+++ libft/libft.h
@@ -16,11 +16,11 @@
 # include <string.h>
 # include <unistd.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
