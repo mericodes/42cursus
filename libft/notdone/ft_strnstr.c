5d8fc097fd9ae452df7d73413a38d91f38fbe9fd
meri codes <itsmemeri@users.noreply.github.com>
2023-05-06 19:18:30 +0000

    makefile bien perron

--- libft/notdone/ft_strnstr.c
+++ libft/notdone/ft_strnstr.c
@@ -9,6 +9,8 @@
 /*   Updated: 2023/05/03 17:21:58 by mlopez-i         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
