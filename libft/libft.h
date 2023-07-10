0ee71aa15a9a52b6b73b8af34d629fea6d335a12
Maria Lopez I Vivas <mlopez-i@car6s5.42barcelona.com>
2023-07-10 18:41:35 +0200

    updated header

--- libft/libft.h
+++ libft/libft.h
@@ -16,28 +16,29 @@
 # include <stdlib.h>
 // # include <string.h>
 
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
 int				ft_isalnum(int c);
+int				ft_isalpha(int c);
 int				ft_isascii(int c);
+int				ft_isdigit(int c);
 int				ft_isprint(int c);
 
-int				atoi (const char * str);
+int				ft_atoi(const char *str);
 
 void			ft_bzero(void *s, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 void			*ft_memset(void *b, int c, size_t len);
-void			*ft_memchr(const void *s, int c, size_t n);
 
-size_t			ft_strlen(const char *s);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strrchr(const char *s, int c);
-int				ft_strncmp(char *s1, char *s2, unsigned int n);
-char			*ft_strnstr(const char *s1, const char *s2, size_t n);
+char			*ft_strdup(const char *s1);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strlen(const char *s);
+int				ft_strncmp(char *s1, char *s2, unsigned int n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *s1, const char *s2, size_t n);
 
 int				ft_tolower(int c);
 int				ft_toupper(int c);
